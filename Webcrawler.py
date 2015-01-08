78c1a35632b0e7b62f1e8dacead886f3d1062d55
Sean MacEachern <ssmaceachern@wpi.edu>
2015-01-08 15:26:37 -0500

    Modified instructions and added TODO for fixing cycles

--- Webcrawler.py
+++ Webcrawler.py
@@ -1,14 +1,14 @@
 #! C:\python27
-#TODO//Add error checking for JavaScript
+#TODO//Add error checking for JavaScript and cycles...
 
 import re, urllib
 
 textfile = file('depth_1.txt','wt')
-print "Enter the URL you wish to crawl..."
-print 'Usage  - "http://www.google.com" <-- With the double quotes'
+print "Enter the URL you wish to crawl in Double Quotes("")..."
+print 'Example  - "http://www.google.com" '
 input_url = input("#>> ")
 
-#Use some regex to parse urls and write all scrawled urls () to a text file
+#Use some regex to parse urls and write all scrawled urls using DFS to a text file
 for link in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(input_url).read(), re.I):
         print link 
         for found_link in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(link).read(), re.I):
